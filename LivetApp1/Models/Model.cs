ce0e114f136f89c032c135da1ffe0827fdb40384
hakase0274 <hakase0274@gmail.com>
2021-01-31 17:35:49 +0900

    ソリューションを追加

---
+++ LivetApp1/Models/Model.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Livet;
+
+namespace LivetApp1.Models
+{
+    public class Model : NotificationObject
+    {
+        private string mText = "HelloWorld";
+        public string BindText 
+        {
+            get => mText;
+            set => RaisePropertyChangedIfSet(ref mText,value);
+        }
+    }
+}
---
+++ LivetApp1/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+
+using Livet;
+using Livet.Commands;
+using Livet.Messaging;
+using Livet.Messaging.IO;
+using Livet.EventListeners;
+using Livet.Messaging.Windows;
+
+using LivetApp1.Models;
+
+namespace LivetApp1.ViewModels
+{
+    public class MainWindowViewModel : ViewModel
+    {
+        public MainWindowViewModel() 
+        {
+            mModel = new Model();
+            var listener = new PropertyChangedEventListener(mModel);
+            listener.RegisterHandler(nameof(Model.BindText), (s,e) => { RaisePropertyChanged(nameof(BindText)); });
+            CompositeDisposable.Add(listener);
+        }
+
+        private Model mModel;
+
+        public string BindText 
+        {
+            get => mModel.BindText;
+            set => mModel.BindText = value;
+        }
+
+        // Some useful code snippets for ViewModel are defined as l*(llcom, llcomn, lvcomm, lsprop, etc...).
+        public void Initialize()
+        {
+        }
+
+    }
+}
